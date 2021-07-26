81fb316dc768d60b6941fa5c6aeab773123e5b7c
Phuong Pham Ngoc <28613589+phuongpntnu@users.noreply.github.com>
2021-07-26 10:51:59 +0700

    Update render_pair_comparison.py

--- render_pair_comparison.py
+++ render_pair_comparison.py
@@ -11,12 +11,12 @@ def main():
     template = env.get_template("pair_comparison.html.jinja2")
 
     html = template.render(
-        page_title="語者判別實驗表單 1",
+        page_title="Biểu mẫu thử nghiệm phân biệt người nói 1",
         form_url="http://localhost:8888",
         form_id=1,
         questions=[
             {
-                "title": "問題 1",
+                "title": "Vấn đề 1",
                 "audio_paths": [
                     "wavs/test1.wav",
                     "wavs/test2.wav"
@@ -24,7 +24,7 @@ def main():
                 "name": "q1"
             },
             {
-                "title": "問題 2",
+                "title": "Vấn đề 2",
                 "audio_paths": [
                     "wavs/test3.wav",
                     "wavs/test4.wav"
